411ca7aada0a60611e3788cfc8a493d487406251
Nguyen Hoang Linh <linh072217@gmail.com>
2021-12-17 13:31:54 +0700

    modify connect to database

--- php/ConnectDB.php
+++ php/ConnectDB.php
@@ -1,17 +1,17 @@
 <?php
     $serverName  = "127.0.0.1";  
-    $userName = "1125926";
-    $passWord = "123LinhDev777123";
-    $dbName = "1125926";
+    $userName = "root";
+    $passWord = "";
+    $dbName = "hoanglinhplus";
 
-function getConnectDB(){
-    $connect = new mysqli($GLOBALS["serverName"],$GLOBALS["userName"],$GLOBALS["passWord"],$GLOBALS["dbName"]);
-    if($connect->connect_error)
-        die("connect failed: ".mysqli_connect_error());
-    else
-        return $connect;
+    function getConnectDB(){
+        $connect = new mysqli($GLOBALS["serverName"],$GLOBALS["userName"],$GLOBALS["passWord"],$GLOBALS["dbName"]);
+        if($connect->connect_error)
+            die("connect failed: ".mysqli_connect_error());
+        else
+            return $connect;
 
-}
+    }
     
    
 ?>
