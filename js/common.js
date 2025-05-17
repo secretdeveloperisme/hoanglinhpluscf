b6b26749fe0dd6cb882670ed0fb79aaf66699129
linhplus <linh072217@gmail.com>
2025-05-17 21:28:14 +0700

    feat: add experiences section

--- js/common.js
+++ js/common.js
@@ -1,4 +1,7 @@
 function removeAd(){
+    let hostname = window.location.hostname;
+    if (hostname === "localhost" || hostname === "127.0.0.1")
+        return;
     let childOfBody = document.body.children;
     let divsOfBody = childOfBody[childOfBody.length-1]
     console.log(document.getElementsByTagName("body")[0].removeChild(divsOfBody));
