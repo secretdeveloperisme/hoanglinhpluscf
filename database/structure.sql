a73a356ae8da938adf2001afaaf1c7cb694930f0
linhplus <linh072217@gmail.com>
2025-05-25 17:15:19 +0700

    feat(posts): desgin table structures for posts

--- database/structure.sql
+++ database/structure.sql
@@ -33,3 +33,64 @@ CREATE TABLE quotes (
     author VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+/* * This SQL script creates a table named 'posts' with the following fields:
+ * - post_id: an auto-incrementing primary key
+ * - title: a string for the post title
+ * - description: a string for the post description
+ * - content: a text field for the post content
+ * - cover_image: a string for the cover image URL
+ * - slug: a string for the post slug (URL-friendly identifier)
+ * - author_id: an integer foreign key referencing the user who authored the post
+ * - is_pulished: a boolean to indicate if the post is published
+ * - created_at: a timestamp for when the post was created
+ * - updated_at: a timestamp for when the post was last updated
+ * - deleted_at: a timestamp for soft deletion of the post
+ */
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+  post_id INT NOT NULL AUTO_INCREMENT,
+  title varchar(255) NOT NULL,
+  description varchar(1000) DEFAULT NULL,
+  content text NOT NULL,
+  cover_image varchar(255) DEFAULT NULL,
+  slug varchar(255) DEFAULT NULL,
+  author_id INT DEFAULT NULL,
+  is_pulished BOOLEAN NOT NULL DEFAULT FALSE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE current_timestamp,
+  deleted_at TIMESTAMP NULL,
+  PRIMARY KEY (post_id),
+  CONSTRAINT FK_AUTHOR FOREIGN KEY (author_id) REFERENCES users (id)
+);
+
+
+-- This SQL script creates a table named 'tags' to categorize posts with the following fields:
+-- - tag_id: an auto-incrementing primary key
+-- - name: a unique string for the tag name
+-- - created_at: a timestamp for when the tag was created
+DROP TABLE IF EXISTS tags;
+CREATE TABLE tags (
+    tag_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- This SQL script creates a junction table named 'post_tags' to establish a many-to-many relationship between posts and tags.
+DROP TABLE IF EXISTS post_tags;
+CREATE TABLE post_tags (
+    post_id INT NOT NULL,
+    tag_id INT NOT NULL,
+
+    PRIMARY KEY (post_id, tag_id),
+
+    FOREIGN KEY (post_id) REFERENCES posts(post_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    
+    FOREIGN KEY (tag_id) REFERENCES tags(tag_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
